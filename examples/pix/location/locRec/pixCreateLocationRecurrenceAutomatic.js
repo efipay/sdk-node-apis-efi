cc595e6037716ee25fdc1d1610826e2614f2b99e
joao-muniz-efi <joao.muniz@sejaefi.com.br>
2025-06-17 18:24:05 +0000

    Release 1.2.24 Corrigido exemplo de uso do método pixCreateLocationRecurrenceAutomatic

--- examples/pix/location/locRec/pixCreateLocationRecurrenceAutomatic.js
+++ examples/pix/location/locRec/pixCreateLocationRecurrenceAutomatic.js
@@ -1,16 +1,9 @@
 const EfiPay = require('sdk-node-apis-efi')
 const options = require('../../../credentials')
 
-let body = {
-    id: 12069,
-    location: 'pix.example.com/qr/v2/rec/2353c790eefb11eaadc10242ac120002',
-    criacao: '2023-12-20T12:38:28.774Z'
-}
-
-
 const efipay = new EfiPay(options)
 
-efipay.pixCreateLocationRecurrenceAutomatic({}, body)
+efipay.pixCreateLocationRecurrenceAutomatic()
     .then((resposta) => {
         console.log(resposta)
     })
